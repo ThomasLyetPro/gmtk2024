31c974dfe194f486ed97e53d603a1d51ee15e395
Thomas Formation <lyet.thomas@gmail.com>
2024-08-20 09:54:34 +0200

    Cell bonus rotation with DOTween

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,9 +1,18 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    private void Start()
+    {
+        DOTween.Sequence().Append(transform.DORotate(new Vector3(0f, 120f, 0f), 1f).SetEase(Ease.Linear))
+            .Append(transform.DORotate(new Vector3(0f, 240f, 0f), 1f).SetEase(Ease.Linear))
+            .Append(transform.DORotate(new Vector3(0f, 360f, 0f), 1f).SetEase(Ease.Linear))
+            .SetLoops(-1);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 6) // Layer is player
